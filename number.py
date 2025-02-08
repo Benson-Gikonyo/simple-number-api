5a886cd1e5d87d8b27b45dc9c009eb920ee6623b
Benson-Gikonyo <muhiugikonyo@gmail.com>
2025-02-09 01:34:39 +0300

    float or -ve

--- number.py
+++ number.py
@@ -13,7 +13,7 @@ def validate(number):
     try:
         number = int(num_str)
         if number < 0 or number is float:
-            return number
+            pass
    
     except ValueError:
         pass
