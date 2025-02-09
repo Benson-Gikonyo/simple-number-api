60ce6eb195ed6e35909b0114eb1d58654c83faa5
Benson-Gikonyo <muhiugikonyo@gmail.com>
2025-02-09 17:53:04 +0300

    added cors

--- number.py
+++ number.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS 
 import requests
 from collections import OrderedDict
 import json
 
 app = Flask(__name__)
+
+CORS(app)
 
 
 # validate
